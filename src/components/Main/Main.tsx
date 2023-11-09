c58ee0b8e664fec8dbc377f46b2adc6d014ac606
MariaMantusova <maria.mantusova@mail.ru>
2023-11-09 15:22:26 +0300

    feat: add footer

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Main.scss";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 function Main() {
     return(
         <>
             <Header path="/learn" linkName="Учить слова"/>
+            <Footer/>
         </>
     )
 }
