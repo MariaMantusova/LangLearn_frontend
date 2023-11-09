d047239aee65083c4c8ed8fd4fd08286061b3f66
MariaMantusova <maria.mantusova@mail.ru>
2023-11-09 18:31:51 +0300

    feat: add start component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,6 +4,7 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import About from "../About/About";
 import WhyWe from "../WhyWe/WhyWe";
+import Start from "../Start/Start";
 
 function Main() {
     return(
@@ -11,6 +12,7 @@ function Main() {
             <Header path="/learn" linkName="Учить слова"/>
             <About/>
             <WhyWe/>
+            <Start/>
             <Footer/>
         </>
     )
