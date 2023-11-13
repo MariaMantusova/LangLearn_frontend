dc6c33d4924aead94441d6863ccf5844e4be4a82
MariaMantusova <maria.mantusova@mail.ru>
2023-11-13 18:05:46 +0300

    feat: addlearning page

--- src/components/LearningPage/LearningPage.tsx
+++ src/components/LearningPage/LearningPage.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import "./LearningPage.scss";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Card from "../Card/Card";
 
 function LearningPage() {
     return(
         <>
+            <Header path="/profile" linkName="В профиль"/>
+            <section className="learning-page">
+                <h1 className="learning-page__title">Мария, у тебя все получится!</h1>
+                <div className="learning-page__container">
+                    <div className="learning-page__vector vector_left"></div>
+                    <Card word="Confused" translation="Озадачен"/>
+                    <div className="learning-page__vector vector_right"></div>
+                </div>
+            </section>
+            <Footer/>
         </>
     )
 }
