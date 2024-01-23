40f6df1f61ae7d165ea7b8b3aaa7659168d1d72d
MariaMantusova <maria.mantusova@mail.ru>
2024-01-23 17:57:42 +0300

    feat: add preloader when adding word

--- src/components/AddingPopup/AddingPopup.tsx
+++ src/components/AddingPopup/AddingPopup.tsx
@@ -1,16 +1,19 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import "./AddingPopup.scss";
 import {IAddingPopupProps} from "../../interfaces/interfacesForProps";
 import {useInput} from "../../hooks/ValidationHook/ValidationHook";
+import Preloader from "../Preloader/Preloader";
 
 function AddingPopup(props: IAddingPopupProps) {
     const word = useInput('', {minLength: 3, isEmpty: true, isEnglishWord: false});
     const translation = useInput('', {minLength: 3, isEmpty: true, isTranslation: false});
+    const [isLoading, setIsLoading] = useState(false);
 
     const isDisabled = !translation.inputValid || !word.inputValid
 
     function handleSubmit(evt: any) {
         evt.preventDefault();
+        setIsLoading(true)
         props.handleWordAdding(word.value, translation.value);
     }
 
@@ -19,38 +22,43 @@ function AddingPopup(props: IAddingPopupProps) {
         translation.setValue("");
         word.setIsDirty(false);
         translation.setIsDirty(false);
+        setIsLoading(false)
     }, [props.isPopupOpen])
 
     return (
         <div className={`adding-popup ${props.isPopupOpen && "adding-popup_visible"}`}>
             <section className="adding-popup__container">
                 <button className="adding-popup__close-icon" onClick={props.onClose}></button>
-                <h2 className="adding-popup__title">Какое слово хочешь добавить?</h2>
-                <form className="adding-popup__form" onSubmit={handleSubmit}>
-                    <div className="adding-popup__input-container">
-                        <input className="adding-popup__input" placeholder="Слово на английском" type="text"
-                               pattern="^[A-Za-z]+$" value={word.value || ""} onBlur={word.onBlur}
-                               onChange={word.onChange}
-                               required/>
-                        {(word.isDirty && word.isEmpty) && <p className="adding-popup__input-error">
-                            Поле не может быть пустым</p>}
-                        {(word.isDirty && word.englishWordError && !word.isEmpty) &&
-                        <p className="adding-popup__input-error">Введите слово на английском</p>}
-                    </div>
-                    <div className="adding-popup__input-container">
-                        <input className="adding-popup__input" placeholder="Перевод" type="text"
-                               onBlur={translation.onBlur}
-                               pattern="^[А-Яа-яЁё]+$" onChange={translation.onChange}
-                               value={translation.value || ""}
-                               required/>
-                        {(translation.isDirty && translation.isEmpty) && <p className="adding-popup__input-error">
-                            Поле не может быть пустым</p>}
-                        {(translation.isDirty && translation.translationError && !translation.isEmpty) &&
-                        <p className="adding-popup__input-error">Введите перевод на русском</p>}
-                    </div>
-                    <button className={`adding-popup__button ${isDisabled && "adding-popup__button_disabled"}` }
-                            disabled={isDisabled}>Добавить слово</button>
-                </form>
+                {isLoading ? <Preloader/> : <>
+                    <h2 className="adding-popup__title">Какое слово хочешь добавить?</h2>
+                    <form className="adding-popup__form" onSubmit={handleSubmit}>
+                        <div className="adding-popup__input-container">
+                            <input className="adding-popup__input" placeholder="Слово на английском" type="text"
+                                   pattern="^[A-Za-z]+$" value={word.value || ""} onBlur={word.onBlur}
+                                   onChange={word.onChange}
+                                   required/>
+                            {(word.isDirty && word.isEmpty) && <p className="adding-popup__input-error">
+                                Поле не может быть пустым</p>}
+                            {(word.isDirty && word.englishWordError && !word.isEmpty) &&
+                            <p className="adding-popup__input-error">Введите слово на английском</p>}
+                        </div>
+                        <div className="adding-popup__input-container">
+                            <input className="adding-popup__input" placeholder="Перевод" type="text"
+                                   onBlur={translation.onBlur}
+                                   pattern="^[А-Яа-яЁё]+$" onChange={translation.onChange}
+                                   value={translation.value || ""}
+                                   required/>
+                            {(translation.isDirty && translation.isEmpty) && <p className="adding-popup__input-error">
+                                Поле не может быть пустым</p>}
+                            {(translation.isDirty && translation.translationError && !translation.isEmpty) &&
+                            <p className="adding-popup__input-error">Введите перевод на русском</p>}
+                        </div>
+                        <button className={`adding-popup__button ${isDisabled && "adding-popup__button_disabled"}`}
+                                disabled={isDisabled}>Добавить слово
+                        </button>
+                    </form>
+                </>
+                }
             </section>
         </div>
     )
